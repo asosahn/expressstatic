c945df71b64c0ca3334d53c64a1506e16bd13241
Andres Sosa <ramonsosadiaz@gmail.com>
2019-10-31 01:30:35 -0500

    node server

--- apps.js
+++ apps.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require("path");
 const app = express();
-
+const pino = require('express-pino-logger')();
+app.use(bodyParser.urlencoded({ extended: false }));
+if (process.env.NODE_ENV === "DEV") {
+  app.use(pino);
+}
 app.use(
   express.static(("../doktoFront/build"), {
     maxAge: 0,
@@ -11,7 +17,13 @@ app.use(
       res.set("Pragma", "no-cache");
     }
   })
-);
+)
+
+const path_ = path.join(`${__dirname}`, '../doktofront/build', 'index.html');
+
+app.get('*', (req, res) => {
+  res.sendFile(path_);
+})
 
 const port = process.env.PORT || 8080;
 
